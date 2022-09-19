d8604b36e83ab2e06f8f33626756d850076f7a3b
Josefine Ahlstedt <josefine.ahlstedt@gmail.com>
2022-09-19 15:45:20 +0200

    Template for suggestion form

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./assets/index.css"
 import HomePage from "./pages/HomePage"
 import NotFoundPage from "./pages/NotFoundPage"
 import NavBar from "./components/NavBar"
+import SuggestionPage from "./pages/SuggestionPage"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="*" element={<NotFoundPage />} />
         <Route path="/" element={<HomePage />} />
+        <Route path="/suggestions" element={<SuggestionPage />} />
       </Routes>
     </div>
   )
