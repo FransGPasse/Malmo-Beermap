e7048dde19c26720a45cb473430bd1efaaa1dadb
Frans Gustavson Påsse <frans.gop@hotmail.com>
2022-09-21 14:28:08 +0200

    Minimal changes in structure on BeerMap and Homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,6 @@
-import { useLoadScript } from "@react-google-maps/api"
 import BeerMap from "../components/BeerMap"
 
 const HomePage = () => {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-  })
-
-  /* Om kartan inte är laddad returnerar vi detta */
-  if (!isLoaded) return <h1>Loading...</h1>
-
-  /* Om allt laddas korrekt, visa kartan */
   return (
     <div className="h-screen w-screen flex flex-col m-auto items-center justify-center space-y-4">
       <h1 className="text-4xl">Malmö BeerMap 🍺</h1>
