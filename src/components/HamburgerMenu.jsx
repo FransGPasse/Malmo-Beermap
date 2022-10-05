64894658a1437e43e260f13938b6c3335465588a
Frans Gustavson Påsse <frans.gop@hotmail.com>
2022-10-05 16:21:24 +0200

    Styling and responsiveness changes

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -2,45 +2,37 @@ import React, { useState } from "react"
 import { Link, NavLink } from "react-router-dom"
 import "../assets/HamburgerMenu.css"
 import Hamburger from "hamburger-react"
-import { gsap } from "gsap"
 
 const HamburgerMenu = () => {
   const [isOpen, setOpen] = useState(false)
 
   return (
-    <>
+    <div className="sm:hidden">
       <Hamburger color="#fbbf24" toggled={isOpen} toggle={setOpen} />
 
       {isOpen && (
-        <ul
-          className="text-accent bg-primary z-60 menu-container
-        "
-        >
-          <li>
-            <NavLink
-              onClick={() => {
-                clicked ? setClicked(false) : setClicked(true)
-              }}
-              as={Link}
-              to="/admin"
-            >
-              Admin
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              onClick={() => {
-                clicked ? setClicked(false) : setClicked(true)
-              }}
-              as={Link}
-              to="/suggestions"
-            >
-              Suggest a bar
-            </NavLink>
-          </li>
-        </ul>
+        <div className="fixed text-accent bg-primary menu-container">
+          <NavLink
+            onClick={() => {
+              clicked ? setClicked(false) : setClicked(true)
+            }}
+            as={Link}
+            to="/admin"
+          >
+            Admin
+          </NavLink>
+          <NavLink
+            onClick={() => {
+              clicked ? setClicked(false) : setClicked(true)
+            }}
+            as={Link}
+            to="/suggestions"
+          >
+            Suggest a bar
+          </NavLink>
+        </div>
       )}
-    </>
+    </div>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,16 @@
 import React from "react"
 import { Link, NavLink } from "react-router-dom"
-import { useAuthContext } from "../contexts/AuthContext"
 import HamburgerMenu from "./HamburgerMenu"
 
 const NavBar = () => {
   return (
-    <div className="navbar bg-primary fixed z-10">
+    <div className="navbar bg-primary fixed z-50">
       <div className="flex-1">
         <a href="/" className="btn btn-ghost normal-case text-xl text-accent">
           Malmö BeerMap 🍻
         </a>
       </div>
-      <div className="hidden md:flex">
+      <div className="hidden sm:flex">
         <ul className="menu menu-horizontal p-0">
           <li>
             <NavLink as={Link} to="/admin">
